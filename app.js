a87ee648bdfd55970bc5faf145623822e333e9b4
Luis <luis.rocha@alumnos.upm.es>
2021-10-07 21:12:10 +0200

    puerto 8080

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 mongoose.connect('mongodb://localhost/API-rest');
 app.get('/', (req, res) => res.send('Hello World'));
 app.use('/api', userRoutes);
-app.listen(3000, () => {
-  console.log('Running on http://localhost:3000');
+app.listen(8080, () => {
+  console.log('Running on http://localhost:8080');
 });
 module.exports = app;
